f3ee2ad3195b8e1a501334e5c36324b5b7d5f372
rezaali <syed.reza.ali@gmail.com>
2015-12-19 19:17:18 +0900

    added files

---
+++ lgp.js
@@ -0,0 +1,11 @@
+//STLS
+exports.stlWriter = require("./lgp/stl-writer");
+exports.stlReader = require("./lgp/stl-reader");
+
+//OBJS
+exports.objWriter = require("./lgp/obj-writer");
+exports.objReader = require("./lgp/obj-reader");
+
+//SVGS
+exports.svgWriter = require("./lgp/svg-writer");
+exports.svgReader = require("./lgp/svg-reader");
