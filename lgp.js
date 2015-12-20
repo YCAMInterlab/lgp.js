c096fb03d7469e1929cba4d4793deb6a8849f38e
rezaali <syed.reza.ali@gmail.com>
2015-12-20 21:30:10 +0900

    removed file readers

--- lgp.js
+++ lgp.js
@@ -1,14 +1,11 @@
 //STLS
 exports.stlSerializer = require("./lgp/stl-serializer");
-exports.stlReader = require("./lgp/stl-reader");
 
 //OBJS
 exports.objSerializer = require("./lgp/obj-serializer");
-exports.objReader = require("./lgp/obj-reader");
 
 //SVGS
 exports.svgSerializer = require("./lgp/svg-serializer");
-exports.svgReader = require("./lgp/svg-reader");
 
 //FILE UTILITY
 exports.fileWriter = require("./lgp/file-writer");
